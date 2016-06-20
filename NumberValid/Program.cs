b98ee02bede4fb9158c01df8fc8394012315e8d1
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-06-20 16:49:13 +1000

    Working code
    This program prompts user to enter a 9 digit number and then checks that
    the number is valid with the modulo-11 method.

---
+++ NumberValid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//
+namespace NumberValid {
+    class Program {
+        static void Main(string[] args) {
+            int[] number = new int[9];
+            Console.Write("Enter your 9 didgit number by pressing enter after each didgit:\n");
+            for (int i = 0; i < 9; i++) {            
+                number[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(NumberValid(number));
+            Console.ReadKey();
+        }
+        static bool NumberValid(int[] number) {
+            int sum = 0;
+            bool modulo = false;
+            for(int j = 0; j < number.Length; j++) {
+                sum = sum + (number[j] * (9-j));               
+            }
+            
+            if (sum % 11 == 0) {
+                modulo = true;
+            }
+            return modulo;
+            
+        }
+    }
+}
