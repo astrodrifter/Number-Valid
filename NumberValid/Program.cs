334eb7c1ce29165e0800ce06c3df70b29236cf95
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-06-20 17:03:38 +1000

    Fixed some spelling.

--- NumberValid/Program.cs
+++ NumberValid/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//
+//modula-11 number validation check
 namespace NumberValid {
     class Program {
         static void Main(string[] args) {
             int[] number = new int[9];
-            Console.Write("Enter your 9 digit number by pressing enter after each didgit:\n");
+            Console.Write("Enter your 9 digit number by pressing" +
+                           "enter after each didgit:\n");
             for (int i = 0; i < 9; i++) {            
                 number[i] = Int32.Parse(Console.ReadLine());
             }
@@ -23,14 +20,14 @@ namespace NumberValid {
         /// otherwise returns false </returns>
         static bool NumberValid(int[] number) {
             int sum = 0;
-            bool modulo = false;
+            bool modula = false;
             for(int j = 0; j < number.Length; j++) {
                 sum = sum + (number[j] * (9-j));               
             }            
             if (sum % 11 == 0) {
-                modulo = true;
+                modula = true;
             }
-            return modulo;          
+            return modula;          
         }
     }
 }
